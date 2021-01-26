07b906826a93e09bcb35f7412c95cc2b0055e38e
britza <bzegarraflores@danielcastelao.org>
2021-01-26 11:16:51 +0100

    segundo commit

--- src/com/HelloWorld/Lider.java
+++ src/com/HelloWorld/Lider.java
@@ -5,5 +5,8 @@ public class Lider {
     public void primercommit(){
         System.out.println("commit 1");
     }
+    public void segundocommit(){
+        System.out.println("commit 2");
+    }
 
 }
