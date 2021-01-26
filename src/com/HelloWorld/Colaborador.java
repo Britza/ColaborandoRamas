e553b05394fc0bed8a10281aecc5e9c6669d7980
David Mariño Alonso <dmarinoalonso@danielcastelao.org>
2021-01-26 11:33:38 +0100

    Segundo cambio

--- src/com/HelloWorld/Colaborador.java
+++ src/com/HelloWorld/Colaborador.java
@@ -5,4 +5,7 @@ public class Colaborador {
     public void primercambio(){
         System.out.println("primer cambio");
     }
+    public void segundocambio(){
+        System.out.println("segundo cambio");
+    }
 }
